cce576362440ca2df7c7b080a0556925d5e2d1f2
hafizmuhammadyahya123 <hafizmuhammadyahya123@gmail.com>
2025-08-18 21:15:59 +0500

    local-remote

---
+++ pythondemo1.py
@@ -0,0 +1,20 @@
+# variables name legal and illegal 
+snake_case_var_for_str = "this is a string.."  #legal var name
+print(snake_case_var_for_str)
+
+PascalCaseVariableForString = "this is a string.." #legal var name
+
+# my num = 20 illlegal var name
+
+# 1num = 20 illegal var name
+
+num1 = 20 #legal var name
+print(num1)
+
+var_for_int = 2  #legal var name
+print(var_for_int)
+
+var_1 = 3 #legal var name
+print(var_1)
+
+
